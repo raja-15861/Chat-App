4bd6f7268fd12fdb474f36dd2363a759ae148aae
Raja kumar <143215884+raja-15861@users.noreply.github.com>
2026-08-07 11:54:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173','https://chat-app-one-umber-68.vercel.app'],
     credentials:true
 }));
 
